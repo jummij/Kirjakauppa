edcb6766957c03c7f8526c8116a69e50ac2ea4a4
Dung <jum.pham@hotmail.com>
2021-09-22 22:33:18 +0300

    list, add, delete ja tyyli. edit puuttuu

--- src/main/java/swd20/Bookstore/domain/Book.java
+++ src/main/java/swd20/Bookstore/domain/Book.java
@@ -17,6 +17,20 @@ public class Book  {
 	private String isbn;
 	private double price;
 	
+
+
+	public Book(Long id, String title, String author, int year, String isbn, double price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	
+
 	public Book(String title, String author, int year, String isbn, double price) {
 		super();
 		this.title = title;
@@ -26,57 +40,66 @@ public class Book  {
 		this.price = price;
 	}
 
+
+
 	public Book() {
 		super();
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public Long getId() {
+		return id;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public String getAuthor() {
 		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 
 	public int getYear() {
 		return year;
 	}
 
+	public void setYear(int year) {
+		this.year = year;
+	}
+
 	public String getIsbn() {
 		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
 	}
 
 	public double getPrice() {
 		return price;
 	}
 
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-				+ "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
-	
-	
+
 	
 	
 
--- src/main/java/swd20/Bookstore/web/BookController.java
+++ src/main/java/swd20/Bookstore/web/BookController.java
@@ -1,14 +1,60 @@
 package swd20.Bookstore.web;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import swd20.Bookstore.domain.Book;
+import swd20.Bookstore.domain.BookRepository;
 
 @Controller
-public class BookController {
+	public class BookController {
 	
-	@RequestMapping("/index") 
+	/*@RequestMapping("/index") 
 	public String index() {
 		return "Tervetuloa";
+	} */
+	
+	@Autowired
+	BookRepository bookRepo;
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookStoreList(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("books", bookRepo.findAll());
+		return "booklist";
 	}
+		
+	@RequestMapping(value = "/add") // new student lomake
+	public String addBook(Model model) {
+			model.addAttribute("book", new Book());
+			return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String addBook(Book book) {
 
+		bookRepo.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+
+		bookRepo.deleteById(id);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long id, @RequestBody Book book) {
+		bookRepo.findById(id);
+		return "editbook";
+	}
 }
