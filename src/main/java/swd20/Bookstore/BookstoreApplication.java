641879fc8a4c5760f9008c8d5ec4d84dc26e86c2
Dung <jum.pham@hotmail.com>
2021-09-22 20:32:03 +0300

    tehtävä 3.1, Add JPA ja H2-database, luotu repository, täydennetty properties ja lisätty testidataa

--- src/main/java/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/swd20/Bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package swd20.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import swd20.Bookstore.domain.Book;
+import swd20.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	//testidatan luonti H2-databaseen sovelluksen käynnistyessä
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepo) {
+		return (args) -> {
+			log.info("saving book data");
+			Book book1 = new Book("Ohana", "Dung Pham", 2018, "293048-12", 10.90);
+			Book book2 = new Book ("Tähtitaivas", "Jane Doe", 1989, "734579-02", 12.50);
+			bookRepo.save(book1);
+			bookRepo.save(book2);
+			
+			log.info("fetch all books");
+			for ( Book book : bookRepo.findAll()) {
+				log.info(book.toString());
+			}
+			
+		};
 	}
 
 }
--- src/main/java/swd20/Bookstore/domain/Book.java
+++ src/main/java/swd20/Bookstore/domain/Book.java
@@ -1,6 +1,16 @@
 package swd20.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book  {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private int year;
