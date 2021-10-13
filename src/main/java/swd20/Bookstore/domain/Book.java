36fed855ce404460dcf53bd4622cac48b5ee836e
Dung <jum.pham@hotmail.com>
2021-10-13 22:27:45 +0300

    securing the web application part 1

--- src/main/java/swd20/Bookstore/domain/Book.java
+++ src/main/java/swd20/Bookstore/domain/Book.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Book  {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String author;
--- src/main/java/swd20/Bookstore/web/BookController.java
+++ src/main/java/swd20/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,6 +61,7 @@ import swd20.Bookstore.domain.BookRepository;
 		return "redirect:booklist";
 	}
 	
+	@PreAuthorize(value = "hasRole('ADMIN')") //vain ADMIN roolilla oikeus poistaa 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 
